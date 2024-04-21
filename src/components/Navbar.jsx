4bfc859340cdc4a8aba858e7cd1c4aee7b9c2e18
Emagomezj <emagomezj@gmail.com>
2024-04-21 20:34:07 -0300

    modificaciones navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,18 +18,13 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">primera gilada</Nav.Link>
+            <Nav.Link href="#features">Equipos</Nav.Link>
             <Nav.Link href="#pricing">segunda gilada</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+            <NavDropdown title="Usuarios" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Mis pacientes
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">General</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
